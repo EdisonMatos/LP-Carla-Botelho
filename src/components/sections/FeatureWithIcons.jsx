f214b2026cdfc652cca35fb9e9f71a28bd3390d9
Edison Matos <edison.matos@live.com>
2025-03-04 06:31:39 -0300

    Initial commit

---
+++ src/components/sections/FeatureWithIcons.jsx
@@ -0,0 +1,148 @@
+import FeatureImgOnBgCard from "../cards/FeatureImgOnBgCard";
+import SectionArea from "../sectionElements/SectionArea";
+import SectionHeader from "../sectionElements/SectionHeader";
+import SectionWrapper from "../sectionElements/SectionWrapper";
+import content from "../../content/content";
+import IconFeatureCard from "../cards/IconFeatureCard";
+import MotionDivDownToUp from "../animation/MotionDivDownToUp";
+import Icon1 from "../../assets/imgs/icons/icon1.png";
+import Icon2 from "../../assets/imgs/icons/icon2.png";
+import Icon3 from "../../assets/imgs/icons/icon3.png";
+import Icon4 from "../../assets/imgs/icons/icon4.png";
+
+export default function FeaturesWithIcons() {
+  return (
+    <SectionArea id="service" className="squares">
+      <SectionHeader
+        className="text-center"
+        miniTitle={content.texts.features.miniTag}
+        sectionHeaderTitle={content.texts.features.title}
+        sectionHeaderSubtitle={content.texts.features.subtitle}
+        color="dark"
+      />
+      <SectionWrapper>
+        <div className="flex flex-col items-center w-full tablet1:flex-row tablet1:justify-between gap-x-[10%] px-[4%] desktop1:gap-x-0 desktop1:px-0">
+          <div className="col1 tablet1:w-[50%] desktop1:w-[28%]">
+            <MotionDivDownToUp>
+              <IconFeatureCard
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-gavel"
+                  >
+                    <path d="m14.5 12.5-8 8a2.119 2.119 0 1 1-3-3l8-8" />
+                    <path d="m16 16 6-6" />
+                    <path d="m8 8 6-6" />
+                    <path d="m9 7 8 8" />
+                    <path d="m21 11-8-8" />
+                  </svg>
+                }
+                title={content.texts.features.card1.title}
+                paragraph={content.texts.features.card1.subtitle}
+                className="tablet1:mb-[46px] desktop1:mb-0 desktop2:mb-[46px]"
+              />
+            </MotionDivDownToUp>
+            <MotionDivDownToUp>
+              <IconFeatureCard
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-siren"
+                  >
+                    <path d="M7 18v-6a5 5 0 1 1 10 0v6" />
+                    <path d="M5 21a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-1a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2z" />
+                    <path d="M21 12h1" />
+                    <path d="M18.5 4.5 18 5" />
+                    <path d="M2 12h1" />
+                    <path d="M12 2v1" />
+                    <path d="m4.929 4.929.707.707" />
+                    <path d="M12 12v6" />
+                  </svg>
+                }
+                title={content.texts.features.card2.title}
+                paragraph={content.texts.features.card2.subtitle}
+              />
+            </MotionDivDownToUp>
+          </div>
+
+          <MotionDivDownToUp className="hidden desktop1:flex justify-center w-[32%]">
+            <div
+              className="hidden h-[640px] w-full desktop1:flex col2 rounded-2xl bg-top bg-cover"
+              style={{
+                backgroundImage: `url(${content.texts.features.imgFeatures})`,
+              }}
+            ></div>
+          </MotionDivDownToUp>
+
+          <div className="col3 tablet1:w-[50%] desktop1:w-[28%]">
+            <MotionDivDownToUp>
+              <IconFeatureCard
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-house"
+                  >
+                    <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                    <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                  </svg>
+                }
+                title={content.texts.features.card3.title}
+                paragraph={content.texts.features.card3.subtitle}
+                className="tablet1:mb-[46px] desktop1:mb-0 desktop2:mb-[46px]"
+              />
+            </MotionDivDownToUp>
+            <MotionDivDownToUp>
+              <IconFeatureCard
+                icon={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-hourglass"
+                  >
+                    <path d="M5 22h14" />
+                    <path d="M5 2h14" />
+                    <path d="M17 22v-4.172a2 2 0 0 0-.586-1.414L12 12l-4.414 4.414A2 2 0 0 0 7 17.828V22" />
+                    <path d="M7 2v4.172a2 2 0 0 0 .586 1.414L12 12l4.414-4.414A2 2 0 0 0 17 6.172V2" />
+                  </svg>
+                }
+                title={content.texts.features.card4.title}
+                paragraph={content.texts.features.card4.subtitle}
+              />
+            </MotionDivDownToUp>
+          </div>
+        </div>
+      </SectionWrapper>
+    </SectionArea>
+  );
+}
